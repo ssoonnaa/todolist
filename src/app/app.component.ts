013e81ce4dabf28da87baa5b62eff0785737f4d6
ssoonnaa <sonalisharma7724@gmail.com>
2022-01-13 21:38:32 +0530

    finally commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,20 +25,34 @@ export class AppComponent {
     //console.log(this.todolistForm.value);
     this.typesOfLists.push(this.todolistForm.value.mylist);
     console.log({item: this.typesOfLists,id:this.typesOfLists.length})
-    this.user=Object.assign(this.user,this.todolistForm.value);
+  
+    fetch('http://localhost:5002/api/todo/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify(this.user)
+
+      body: JSON.stringify(this.typesOfLists)
+
+    }).then(function (res: any) {
+      //console.log("DDDDDDDDD" + res.status);
+      if (res.status == 200) {
+        console.log("data added successfully");
+      }
+      else {
+        res.json().then(function (data: any) {
+          console.log("something went wrong " + res.status);
+        })
+      }
+    })
+      /*this.user=localStorage.setItem('mydata',JSON.stringify(this.todolistForm.value));
     this.addmultipleuser(this.user);
       }
-    addmultipleuser(user: any){
+    addmultipleuser(userss: any){
       
-     let users =[];
-      if(localStorage.getItem('mydata')){
-        var text = localStorage.getItem('mydata');
-        users=JSON.parse(user)
-        users=[user,...users]
-      }  else{
-        users=[user];
-      }
-      localStorage.setItem('mydata',JSON.stringify(user))
+     let users =JSON.parse(localStorage.getItem('mydata')|| '{}');
+  */
       }
     delete(){ 
            this.typesOfLists=this.typesOfLists.filter(item=>this.typesOfLists.length!==this.typesOfLists.length)
