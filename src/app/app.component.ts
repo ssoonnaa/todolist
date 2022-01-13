6d4f69660c63bbe5b95fed322ef14de15da143d2
ssoonnaa <sonalisharma7724@gmail.com>
2022-01-13 18:34:39 +0530

    ok commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ const elementdata:todolists[]=[
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';   
+  title = 'todolist';
+  user:any={};
  todolistForm = new FormGroup({
     mylist: new FormControl('', [Validators.required]),
   });
@@ -24,7 +25,21 @@ export class AppComponent {
     //console.log(this.todolistForm.value);
     this.typesOfLists.push(this.todolistForm.value.mylist);
     console.log({item: this.typesOfLists,id:this.typesOfLists.length})
-    }
+    this.user=Object.assign(this.user,this.todolistForm.value);
+    this.addmultipleuser(this.user);
+      }
+    addmultipleuser(user: any){
+      
+     let users =[];
+      if(localStorage.getItem('mydata')){
+        var text = localStorage.getItem('mydata');
+        users=JSON.parse(user)
+        users=[user,...users]
+      }  else{
+        users=[user];
+      }
+      localStorage.setItem('mydata',JSON.stringify(user))
+      }
     delete(){ 
            this.typesOfLists=this.typesOfLists.filter(item=>this.typesOfLists.length!==this.typesOfLists.length)
     }
